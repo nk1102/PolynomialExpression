136d895336d533bb31cb73e05f658d6c094d6ba5
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-24 10:46:26 +0530

    created boolean isempty() method to check the given list is empty or not

--- src/implementation/MyPolynomial.java
+++ src/implementation/MyPolynomial.java
@@ -3,8 +3,14 @@ package implementation;
 public class MyPolynomial {
     private Node head;
     private Node tail;
-    private int size ;
-    public Node getHead(){
-        return head ;
+    private int size;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public boolean isempty() {
+        return head == null;
+    }
 }
-}
+
