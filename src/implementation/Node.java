48dd90d6a2d5fd7314d6afe9008f744e604b5f05
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-24 10:40:43 +0530

    created getter and getter methods for the Node class

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -10,4 +10,28 @@ public class Node {
         this.power = power;
         this.next = next;
     }
+
+    public int getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(int coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
