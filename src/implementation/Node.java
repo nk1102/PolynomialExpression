e32c493854ad1523c0655c6b950aeee2bf95c0cc
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-24 10:39:45 +0530

    created default constructor of the Node class

--- src/implementation/Node.java
+++ src/implementation/Node.java
@@ -5,5 +5,9 @@ public class Node {
     private int power;
     private Node next;
 
-
+    public Node(int coefficient, int power, Node next) {
+        this.coefficient = coefficient;
+        this.power = power;
+        this.next = next;
+    }
 }
