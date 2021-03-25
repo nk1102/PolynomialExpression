b127ce35456117738f634c26b6b24187fea52e66
nikunj <nikunj.khandelwal_cs19@gla.ac.in>
2021-03-25 11:15:57 +0530

    created linkedList2 object and added the second polynomial in the linked list

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -10,5 +10,10 @@ public  class MyMain {
         linkedList1.addLast(2, 1);
         linkedList1.addLast(1, 0);
         linkedList1.traverse();
+        polynomial linkedList2 = new polynomial();
+        linkedList2.addLast(-2,3);
+        linkedList2.addLast(-9,2);
+        linkedList2.addLast(2,0);
+        linkedList2.traverse();
     }
 }
